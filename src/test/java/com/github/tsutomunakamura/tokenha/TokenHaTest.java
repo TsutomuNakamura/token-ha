a1db6dd0f8112ef37e418941b9bf0917d7e02220
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-08-17 11:18:42 +0900

    Removed temporaly test cases

--- src/test/java/com/github/tsutomunakamura/tokenha/TokenHaTest.java
+++ src/test/java/com/github/tsutomunakamura/tokenha/TokenHaTest.java
@@ -15,20 +15,6 @@ import java.util.Iterator;
  */
 public class TokenHaTest {
     @Test
-    public void testTest() {
-        Deque<TokenElement> fifoQueue = new ArrayDeque<>();
-        fifoQueue.add(new TokenElement("token1", System.currentTimeMillis()));
-        fifoQueue.add(new TokenElement("token2", System.currentTimeMillis() + 60));
-        fifoQueue.add(new TokenElement("token3", System.currentTimeMillis() + 120));
-
-        Iterator<TokenElement> iterator = fifoQueue.descendingIterator();
-        iterator.forEachRemaining(element -> {
-            System.out.println("Token: " + element.getToken() + ", Time: " + element.getTimeMillis());
-        });
-        System.out.println("Finished iterating through the queue.");
-    }
-
-    @Test
     public void testToJsonWithEmptyQueue() {
         try (TokenHa tokenHa = new TokenHa()) {
             String json = tokenHa.toJson();
