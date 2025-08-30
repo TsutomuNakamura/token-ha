3046e2300453c9ff13e341dca9d7c1adfe3d74b6
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-08-30 23:14:56 +0900

    Updated

--- src/test/java/com/github/tsutomunakamura/tokenha/EvictionThreadConfigTest.java
+++ src/test/java/com/github/tsutomunakamura/tokenha/EvictionThreadConfigTest.java
@@ -2,9 +2,18 @@ package com.github.tsutomunakamura.tokenha;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
+
+@ExtendWith(SystemStubsExtension.class)
 public class EvictionThreadConfigTest {
     
+    @SystemStub
+    private EnvironmentVariables environmentVariables;
+
     // Test cases for toBuilder() method
 
     @Test
@@ -31,7 +40,7 @@ public class EvictionThreadConfigTest {
     // Test cases for fromProperties() method
     
     @Test
-    @DisplayName("Builder should set initialDelayMillis with defauilt value when builder.initialDelayMillis(initialDelay) throws IllegalArgumentException")
+    @DisplayName("fromProperties() should set initialDelayMillis with defauilt value when builder.initialDelayMillis(initialDelay) throws IllegalArgumentException")
     void testFromPropertiesWithInvalidInitialDelay() {
         // Given properties with invalid initial delay
         java.util.Properties props = new java.util.Properties();
@@ -47,7 +56,7 @@ public class EvictionThreadConfigTest {
     }
 
     @Test
-    @DisplayName("Builder should set intervalMillis with default value when builder.intervalMillis(interval) throws IllegalArgumentException")
+    @DisplayName("fromProperties() should set intervalMillis with default value when builder.intervalMillis(interval) throws IllegalArgumentException")
     void testFromPropertiesWithInvalidInterval() {
         // Given properties with invalid interval
         java.util.Properties props = new java.util.Properties();
@@ -59,6 +68,38 @@ public class EvictionThreadConfigTest {
 
         // Then - verify that default is used for invalid interval
         assert config.getInitialDelayMillis() == 3000; // From properties
+        assert config.getIntervalMillis() == 10000; // Default value
+    }
+
+    // Test cases for fromEnvironment() method
+
+    @Test
+    @DisplayName("fromEnvironment() should set initialDelayMillis with default value when builder.initialDelayMillis(initialDelay) throws IllegalArgumentException")
+    void testFromEnvironmentWithInvalidInitialDelay() {
+        // Fake environment variables by using SystemStubs.
+        // https://www.baeldung.com/java-system-stubs
+        environmentVariables.set("TOKENHA_EVICTION_INITIAL_DELAY_MILLIS", "-1000"); // Invalid negative value
+        environmentVariables.set("TOKENHA_EVICTION_INTERVAL_MILLIS", "25000");
+        // When
+        EvictionThreadConfig config = EvictionThreadConfig.fromEnvironment();
+        // Then - verify that default is used for invalid initial delay
+        assert config.getInitialDelayMillis() == 1000; // Default value
+        assert config.getIntervalMillis() == 25000; // From environment
+    }
+
+    @Test
+    @DisplayName("fromEnvironment() should set intervalMillis with default value when builder.intervalMillis(interval) throws IllegalArgumentException")
+    void testFromEnvironmentWithInvalidInterval() {
+        // Fake environment variables by using SystemStubs.
+        // https://www.baeldung.com/java/system-stubs
+        environmentVariables.set("TOKENHA_EVICTION_INITIAL_DELAY_MILLIS", "4000");
+        environmentVariables.set("TOKENHA_EVICTION_INTERVAL_MILLIS", "-2000"); // Invalid negative value
+
+        // When
+        EvictionThreadConfig config = EvictionThreadConfig.fromEnvironment();
+
+        // Then - verify that default is used for invalid interval
+        assert config.getInitialDelayMillis() == 4000; // From environment
         assert config.getIntervalMillis() == 10000; // Default value
     }
 }
